b9f69c900c08bc90908dd2521b11f00ac416d9b5
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2024-01-05 20:41:40 +0800

    init

---
+++ OrderFusion/src/main/java/tech/hirsun/orderfusion/orderfusion/OrderFusionApplication.java
@@ -0,0 +1,13 @@
+package tech.hirsun.orderfusion.orderfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFusionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFusionApplication.class, args);
+    }
+
+}
---
+++ OrderFusion/src/test/java/tech/hirsun/orderfusion/orderfusion/OrderFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hirsun.orderfusion.orderfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
