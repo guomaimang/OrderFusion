235eead148b10ffff2d6d11af87e646c486f4dcc
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2024-01-07 14:02:24 +0800

    update

--- src/test/java/tech/hirsun/orderfusion/OrderFusionApplicationTests.java
+++ src/test/java/tech/hirsun/orderfusion/OrderFusionApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrderFusionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
